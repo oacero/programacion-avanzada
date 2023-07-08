68980eb649ceb95743116bc8edddac57cb75cc08
Oscar Acero <oskargiovanny@gmail.com>
2023-07-07 20:03:46 -0500

    Class & Objects

---
+++ ClasesObjetos/src/Main.java
@@ -0,0 +1,118 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        public String nombre = new String();
+
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+
+/*
+
+El nombre de la clase debe empezar con Mayuscula
+
+AutosModernos
+
+modificado_acceso class NombreClase{
+
+}
+
+Ejemplo:
+
+public class Componentes{
+
+}
+
+Dentro de la clase se distinguen 2 partes
+- Los atributos (pr4opiedads y/o caracteristicas )
+- Los Métodos (acciones que realiza el objeto)
+
+
+Atributos
+
+modificador_acceso tipo nombreAtributo = valor;
+
+Ej:
+
+private int codigo = 001;
+public String nombre = 'Oscar';
+
+Métodos
+
+modificador_acceso tipo_retorno nombremetodo(tipo nomParametro, tipo nomParametro2....){
+
+}
+
+Ejemplo:
+public int sumar(int num1, int num2) {
+    return num1 + mum2;
+}
+
+Cronstructores
+Es un tipo de metodo especial que permite la construccion de objetos (instancia)
+La finalidad es inicializar los atributos
+
+El constructor tiene las siguientes caracteristicas
+- No tiene tipo de retorno
+- Su nombre tiene que ser igual al de la Clase
+
+modificador_acceso Nombre(tipo nomParametro, tipo nomParametro2, ....){
+}
+
+Ejemplo
+public Main(){
+
+}
+
+public Main(int nombre){
+}
+
+El ejemplo anterior muestra la caracteristica de sobrecarga de metodos
+
+Convenciones de Java
+
+- Nombre de clases siempre empiezan con mayuscula
+ Ej: MiCarro, String, Animal, Electrodomesticos
+
+- Nombre de atributos empiezan con minuscula
+  Ej: nombrePersona, numero, contador, estado, esCadena
+
+- Nombre de metodos empieza con minuscula, excepto los constructores que empiezan con mayuscula
+Ej: sumar, ascelerar, escribirCuenta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
